79b21806afb472f9df3e5ed0e41199c388ac31c5
Arthur Ishmiev <artur.ishmiev@gmail.com>
2021-12-14 19:41:59 +0500

    2-array-objects-hw

--- projects/array-objects/index.js
+++ projects/array-objects/index.js
@@ -9,7 +9,11 @@
  Пример:
    forEach([1, 2, 3], (el) => console.log(el))
  */
-function forEach(array, fn) {}
+function forEach(array, fn) {
+  for (let i = 0; i < array.length; i++) {
+    fn(array[i], i, array);
+  }
+}
 
 /*
  Задание 2:
@@ -20,7 +24,13 @@ function forEach(array, fn) {}
  Пример:
    map([1, 2, 3], (el) => el ** 2) // [1, 4, 9]
  */
-function map(array, fn) {}
+function map(array, fn) {
+  const resultArray = [];
+  for (let i = 0; i < array.length; i++) {
+    resultArray.push(fn(array[i], i, array));
+  }
+  return resultArray;
+}
 
 /*
  Задание 3:
@@ -31,7 +41,24 @@ function map(array, fn) {}
  Пример:
    reduce([1, 2, 3], (all, current) => all + current) // 6
  */
-function reduce(array, fn, initial) {}
+function reduce(array, fn, initial) {
+  let i = 0;
+  let accumulator;
+
+  if (initial === undefined) {
+    accumulator = array[0];
+    i = 1;
+  } else {
+    accumulator = initial;
+    i = 0;
+  }
+
+  for (i; i < array.length; i++) {
+    accumulator = fn(accumulator, array[i], i, array);
+  }
+
+  return accumulator;
+}
 
 /*
  Задание 4:
@@ -41,7 +68,15 @@ function reduce(array, fn, initial) {}
  Пример:
    upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
  */
-function upperProps(obj) {}
+function upperProps(obj) {
+  const result = [];
+
+  for (const name in obj) {
+    result.push(name.toUpperCase());
+  }
+
+  return result;
+}
 
 /*
  Задание 5 *:
@@ -54,6 +89,17 @@ function upperProps(obj) {}
    obj.foo = 2;
    console.log(obj.foo); // 4
  */
-function createProxy(obj) {}
+function createProxy() {
+  const proxy = new Proxy(
+    {},
+    {
+      set: function (target, prop, value) {
+        target[prop] = value ** 2;
+        return true;
+      },
+    }
+  );
+  return proxy;
+}
 
 export { forEach, map, reduce, upperProps, createProxy };
