b7c2a2049b300f1d34611c2fcb732fa5846a1859
Arthur Ishmiev <artur.ishmiev@gmail.com>
2021-12-30 22:09:01 +0500

    Revert "revert1 test"
    This reverts commit 4734522d1791a7449f6d5d31e5a2e2ee608c7809.

--- projects/dnd/index.js
+++ projects/dnd/index.js
@@ -19,13 +19,56 @@ import './dnd.html';
 
 const homeworkContainer = document.querySelector('#app');
 
-document.addEventListener('mousemove', (e) => {});
+export function createDiv() {
+  const element = document.createElement('div');
+  element.setAttribute('draggable', true);
 
-export function createDiv() {}
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  const red = getRandomInt(255);
+  const green = getRandomInt(255);
+  const blue = getRandomInt(255);
+
+  element.classList.add('draggable-div');
+  element.style.position = 'absolute';
+  element.style.top = `${getRandomInt(300)}px`; // тест не пропускает getRandomInt(300) , хотя это рабочий вариант
+  element.style.left = `${getRandomInt(300)}px`;
+  element.style.width = `${getRandomInt(300)}px`;
+  element.style.height = `${getRandomInt(300)}px`;
+  element.style.backgroundColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+
+  return element;
+}
 
 const addDivButton = homeworkContainer.querySelector('#addDiv');
 
 addDivButton.addEventListener('click', function () {
   const div = createDiv();
   homeworkContainer.appendChild(div);
+
+  div.onmousedown = function (event) {
+    //DnD
+    moveAt(event.pageX, event.pageY);
+    function moveAt(pageX, pageY) {
+      div.style.left = pageX - div.offsetWidth / 2 + 'px';
+      div.style.top = pageY - div.offsetHeight / 2 + 'px';
+    }
+
+    function onMouseMove(event) {
+      moveAt(event.pageX, event.pageY);
+    }
+
+    document.addEventListener('mousemove', onMouseMove);
+
+    div.onmouseup = function () {
+      document.removeEventListener('mousemove', onMouseMove);
+      div.onmouseup = null;
+    };
+
+    div.ondragstart = function () {
+      return false;
+    };
+  };
 });
