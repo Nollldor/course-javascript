c4dd26c73f9928324d34950720df737b99cf9d4a
Arthur Ishmiev <artur.ishmiev@gmail.com>
2021-12-30 21:57:33 +0500

    Revert "6-dnd hw after correction"
    This reverts commit 7d91baa4f80bbb7e0b86b71b16a2e19076efa9cb.
    
     Changes to be committed:

--- projects/dnd/functions.js
+++ projects/dnd/functions.js
@@ -79,11 +79,13 @@ function delegate(target, fn) {
    once(document.querySelector('button'), () => console.log('обработчик выполнился!')) // добавит такой обработчик кликов для указанного элемента, который вызовется только один раз и затем удалится
  */
 function once(target, fn) {
-  function clicked(e) {
-    fn();
-    target.removeEventListener('click', clicked, true);
-  }
-  target.addEventListener('click', clicked, true);
+  target.addEventListener(
+    'click',
+    () => {
+      fn();
+    },
+    { once: true }
+  );
 }
 
 export { addListener, removeListener, skipDefault, emulateClick, delegate, once };
